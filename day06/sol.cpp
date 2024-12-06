c68da5048619bbbd96dfd57f3998215d20814879
y3shiy <y3shiy@gmail.com>
2024-12-06 14:43:57 +0100

    Day 6 solved

--- day06/sol.cpp
+++ day06/sol.cpp
@@ -3,12 +3,46 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+#include <functional>
 
 using namespace std::string_literals;
 
 auto g_dr = std::array<int32_t, 4>{-1, +0, +1, +0};
 auto g_dc = std::array<int32_t, 4>{+0, +1, +0, -1};
 auto g_ch_repr = std::array<char, 4>{'^', '>', 'v', '<'};
+
+// Returns whether there was a loop or not
+auto will_guard_get_stuck(
+	std::vector<std::string> &grid,
+	int32_t r,
+	int32_t c,
+	int32_t dir,
+	std::function<void(std::vector<std::string>*, int32_t, int32_t, int32_t)> fn)
+	{
+	fn(&grid, r, c, dir);
+	while (true) {
+		r += g_dr[dir];
+		c += g_dc[dir];
+		if (!(0 <= r && r < grid.size())
+			|| !(0 <= c && c < grid.front().size())) {
+			break;
+		}
+		if (grid[r][c] == g_ch_repr[dir]) {
+			return true;
+		}
+		if (grid[r][c] == '#') {
+			r -= g_dr[dir];
+			c -= g_dc[dir];
+			if (grid[r][c] == 'X' || grid[r][c] == '.' || grid[r][c] == '#') {
+				grid[r][c] = g_ch_repr[dir];
+			}
+			dir = (dir + 1) % g_ch_repr.size();
+		} else {
+			fn(&grid, r, c, dir);
+		}
+	}
+	return false;
+}
 
 int main() {
 	auto input_file = std::ifstream("input.txt");
@@ -24,13 +58,14 @@ int main() {
 	auto i = 0;
 	while (std::getline(input_file, str) && str != "") {
 		for (int32_t j = 0; j < str.size(); ++j) {
-			if (str[j] == '^' || str[j] == '>' || str[j] == 'v' || str[j] == '<') {
-				guard_r = i;
-				guard_c = j;
-				auto it = std::find(g_ch_repr.begin(), g_ch_repr.end(), str[j]);
-				guard_dir = std::distance(g_ch_repr.begin(), it); 
-				str[j] = '.';
+			auto it = std::find(g_ch_repr.begin(), g_ch_repr.end(), str[j]);
+			if (it == g_ch_repr.end()) {
+				continue;
 			}
+			guard_r = i;
+			guard_c = j;
+			guard_dir = std::distance(g_ch_repr.begin(), it); 
+			str[j] = '.';
 		}
 		grid.push_back(std::move(str));
 		i++;
@@ -44,31 +79,53 @@ int main() {
 		<< " guard_c = " << guard_c
 		<< " guard_dr = " << g_dr[guard_dir]
 		<< " guard_dc = " << g_dc[guard_dir] << "\n";
+	auto grid_with_xs = grid;
 
-	grid[guard_r][guard_c] = 'X';
-	while (true) {
-		guard_r += g_dr[guard_dir];
-		guard_c += g_dc[guard_dir];
-		if (!(0 <= guard_r && guard_r < grid.size())
-			|| !(0 <= guard_c && guard_c < grid.front().size())) {
-			break;
-		}
-		if (grid[guard_r][guard_c] == '#') {
-			std::cout << "Rotation due to the obstacle at ("
-				<< guard_r << ", " << guard_c << ")\n";
-			guard_r -= g_dr[guard_dir];
-			guard_c -= g_dc[guard_dir];
-			guard_dir = (guard_dir + 1) % g_ch_repr.size();
-		} else {
-			grid[guard_r][guard_c] = 'X';
-		}
+	auto is_there_loop = will_guard_get_stuck(grid_with_xs, guard_r, guard_c, guard_dir,
+		[](auto grid, auto r, auto c, auto dir) {
+			if ((*grid)[r][c] == '.') {
+				(*grid)[r][c] = 'X';
+			}
+		});
+	if (is_there_loop) {
+		std::cerr << "Unreachable!\n";
+		return 1;
 	}
 
 	auto x_count = int64_t{0};
-	for (auto str : grid) {
-		x_count += std::count(str.begin(), str.end(), 'X');
-		std::cout << str << "\n";
+	for (auto str : grid_with_xs) {
+		x_count += std::count_if(str.begin(), str.end(),
+			[](auto x) {
+				return x != '.' && x != '#';
+			});
 	}
 
 	std::cout << "Count of X's: " << x_count << "\n";
+
+	auto possible_obstruction_placement_count = int64_t{0};
+	for (int32_t i = 0; i < grid_with_xs.size(); ++i) {
+		for (int32_t j = 0; j < grid_with_xs.front().size(); ++j) {
+			if (grid_with_xs[i][j] == '.' || grid_with_xs[i][j] == '#') {
+				continue;
+			}
+			if (i == guard_r && j == guard_c) {
+				continue;
+			}
+			auto grid_copy = grid;
+			grid_copy[i][j] = '#';
+
+			auto is_there_loop = will_guard_get_stuck(
+				grid_copy,
+				guard_r,
+				guard_c,
+				guard_dir,
+				[](auto grid, auto r, auto c, auto dir){});
+
+			if (is_there_loop) {
+				possible_obstruction_placement_count++;
+			}
+		}
+	}
+	std::cout << "Count of possible placements of an obstruction: "
+		<< possible_obstruction_placement_count << "\n";
 }
