c02f7d4bb90e133a7ddcbd14412e235fc39ada77
y3shiy <y3shiy@gmail.com>
2024-12-08 19:05:18 +0100

    Solution refactored / made smarter

--- day07/sol.cpp
+++ day07/sol.cpp
@@ -7,42 +7,22 @@
 
 using namespace std::string_literals;
 
-enum Operation {
-	OP_ADD,
-	OP_MUL,
-	OP_TOTAL_CNT
-};
-
 auto g_nums = std::vector<int32_t>();
-auto g_arrangement = std::vector<Operation>();
-auto g_target_value = int64_t{};
-auto g_calculated_value = int64_t{};
 
-auto calc() {
-	auto res = int64_t{g_nums[0]};
-	for (int32_t i = 0; i < g_arrangement.size(); ++i) {
-		switch (g_arrangement[i]) {
-			case OP_ADD: res += g_nums[i+1]; break;
-			case OP_MUL: res *= g_nums[i+1]; break;
-			default: std::cerr << "Unreachable!\n";
-		}
+auto is_possible(int32_t k, int64_t desired_res) {
+	if (k <= 0) {
+		return g_nums[0] == desired_res;
 	}
-	return res;
-}
-
-void rec(int32_t k) {
-	if (k >= g_nums.size() - 1) {
-		g_calculated_value = calc();
-		return;
+	if (g_nums[k] != 0
+		&& desired_res % g_nums[k] == 0
+		&& is_possible(k - 1, desired_res / g_nums[k]))
+		{
+		return true;
 	}
-	for (int32_t i = 0; i < OP_TOTAL_CNT; ++i) {
-		g_arrangement.push_back(static_cast<Operation>(i));
-		rec(k + 1);
-		if (g_calculated_value == g_target_value) {
-			return;
-		}
-		g_arrangement.pop_back();
+	if (is_possible(k - 1, desired_res - g_nums[k])) {
+		return true;
 	}
+	return false;	
 }
 
 int main() {
@@ -54,8 +34,7 @@ int main() {
 	auto str = ""s;
 	auto calibration_res = int64_t{0};
 	while (std::getline(input_file >> std::ws, str) && str != "") {
-		g_target_value = std::atoll(str.data());
-		std::cout << "Target value: " << g_target_value << "\n";
+		auto target_value = std::atoll(str.data());
 		auto pos = std::distance(
 			str.begin(),
 			std::find(str.begin(), str.end(), ':') + 2);
@@ -64,19 +43,8 @@ int main() {
 		for (auto x : std::views::split(sv, ' ')) {
 			g_nums.push_back(std::atoi(x.data()));
 		}
-		g_arrangement.clear();
-		g_arrangement.reserve(g_nums.size() - 1);
-		std::cout << "Nums: ";
-		for (auto x : g_nums) {
-			std::cout << x << " ";
-		}
-		std::cout << "\n";
-		rec(0);
-		if (g_calculated_value == g_target_value) {
-			std::cout << "Yes!\n";
-			calibration_res += g_target_value;
-		} else {
-			std::cout << "No.\n";
+		if (is_possible(g_nums.size() - 1, target_value)) {
+			calibration_res += target_value;
 		}
 	}
 	std::cout << "Total calibration result: " << calibration_res << "\n";
